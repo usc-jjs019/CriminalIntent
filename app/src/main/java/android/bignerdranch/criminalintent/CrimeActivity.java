97d0bac7622e30209b26c6a8253cbba2f6f828e8
usc-jjs019 <jjs019@student.usc.edu.au>
2019-08-14 16:29:28 +1000

    Criminal Intent code

---
+++ app/src/main/java/android/bignerdranch/criminalintent/CrimeActivity.java
@@ -0,0 +1,25 @@
+package android.bignerdranch.criminalintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+public class CrimeActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
+        }
+    }
+}
