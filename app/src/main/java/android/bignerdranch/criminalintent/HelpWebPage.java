f92b84583eaed1d94818b1ad446b992f1b9d02a0
usc-jjs019 <jjs019@student.usc.edu.au>
2019-09-25 18:14:03 +1000

    Finished Complete Criminal Intent App.

--- app/src/main/java/android/bignerdranch/criminalintent/HelpWebPage.java
+++ app/src/main/java/android/bignerdranch/criminalintent/HelpWebPage.java
@@ -32,6 +32,6 @@ public class HelpWebPage extends AppCompatActivity {
             }
         });
 
-        mWebView.loadUrl("www.usc.edu.au");
+        mWebView.loadUrl("https://www.usc.edu.au");
     }
 }
